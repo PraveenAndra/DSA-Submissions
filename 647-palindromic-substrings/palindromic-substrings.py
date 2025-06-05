ad522ca23c11a8f54306b6f19cd5f67ce7907b7b
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-05 16:15:33 -0400

    Time: 178 ms (34.10%) | Memory: 26.1 MB (11.35%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -3,20 +3,37 @@ class Solution:
         l = len(s)
         dp = [[False for i in range(l)] for i in range(l)]
         # print(dp)
-        count = l
-        for i in range(l):
-            dp[i][i] = True
+        count = 0
+        # for i in range(l):
+        #     dp[i][i] = True
 
-        for i in range(l-1):
-            if s[i]==s[i+1]:
+        # for i in range(l-1):
+        #     if s[i]==s[i+1]:
+        #         count += 1
+        #         dp[i][i+1] = True
+        # for diff in range(2,l):
+        #     for i in range(l-diff):
+        #         j = i+diff
+        #         if s[i] == s[j] and dp[i+1][j-1]:
+        #             dp[i][j] = True
+        #             count += 1
+        # return count
+        for i in range(l):
+            # odd
+            lef,r = i,i
+            while lef >=0 and r < l and s[lef]==s[r]:
+                lef -= 1
+                r += 1
                 count += 1
-                dp[i][i+1] = True
-        for diff in range(2,l):
-            for i in range(l-diff):
-                j = i+diff
-                if s[i] == s[j] and dp[i+1][j-1]:
-                    dp[i][j] = True
-                    count += 1
+            #even
+            lef,r = i,i+1
+            while lef>=0 and r<l and s[lef]==s[r]:
+                lef -= 1
+                r += 1
+                count += 1
         return count
+            
+            
+
                  
         
