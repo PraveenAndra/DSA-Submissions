359f50b8df59f9a72bda9e347dc78562d4568b08
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-24 18:37:18 -0400

    Time: 15 ms (50.63%) | Memory: 22.7 MB (13.10%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,9 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # sorting
+        # lookup = defaultdict(list)
+        # for i in strs:
+        #     s = "".join(sorted(i))
+        #     lookup[s].append(i)
+        # return list(lookup.values())
+        
         lookup = defaultdict(list)
         for i in strs:
-            s = "".join(sorted(i))
-            lookup[s].append(i)
+            l = [0]*26
+            for j in i:
+                l[ord(j) - ord('a')] += 1
+            lookup[tuple(l)].append(i)
         return list(lookup.values())
-        
