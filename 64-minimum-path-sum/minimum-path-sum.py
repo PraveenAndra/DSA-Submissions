cac57e9dde180abf79ada59a037cac71942abbcc
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-24 20:17:50 -0400

    Time: 20 ms (32.65%) | Memory: 20.8 MB (21.57%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -20,14 +20,26 @@ class Solution:
         # return -1
                 
         # DP
+        # r,c = len(grid),len(grid[0])
+        # for i in range(r-1,-1,-1):
+        #     for j in range(c-1,-1,-1):
+        #         if i==r-1 and j!=c-1:
+        #             grid[i][j] = grid[i][j] + grid[i][j+1]
+        #         elif i!=r-1 and j==c-1:
+        #             grid[i][j] = grid[i][j] + grid[i+1][j]  
+        #         elif i!=r-1 and j!=c-1:
+        #             grid[i][j] = grid[i][j] + min(grid[i+1][j],grid[i][j+1])
+        # return grid[0][0]
+
         r,c = len(grid),len(grid[0])
-        for i in range(r-1,-1,-1):
-            for j in range(c-1,-1,-1):
-                if i==r-1 and j!=c-1:
-                    grid[i][j] = grid[i][j] + grid[i][j+1]
-                elif i!=r-1 and j==c-1:
-                    grid[i][j] = grid[i][j] + grid[i+1][j]  
-                elif i!=r-1 and j!=c-1:
-                    grid[i][j] = grid[i][j] + min(grid[i+1][j],grid[i][j+1])
-        return grid[0][0]
+        for i in range(r):
+            for j in range(c):
+                if i==0 and j!=0:
+                    grid[i][j] = grid[i][j] + grid[i][j-1]
+                elif i!=0 and j==0:
+                    grid[i][j] = grid[i][j] + grid[i-1][j]  
+                elif i!=0 and j!=0:
+                    grid[i][j] = grid[i][j] + min(grid[i-1][j],grid[i][j-1])
+        print(grid)
+        return grid[r-1][c-1]
             
