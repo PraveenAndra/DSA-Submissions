f84b04190432f8b170a4c2c6512bab3aa76375cc
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-25 13:38:10 -0400

    Time: 252 ms (50.59%) | Memory: 20.1 MB (82.39%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def bfs(gridMap,x,y):
-            q = deque()
-            q.append((x,y))
-            while q:
-                a,b = q.popleft()
-                for i,j in [(0,1),(-1,0),(0,-1),(1,0)]:
-                    nx,ny = a+i,b+j
-                    if 0<=nx<len(grid) and 0<=ny<len(grid[0]) and gridMap[nx][ny] == '1':
-                        gridMap[nx][ny] = 0
-                        q.append((nx,ny))
-            return gridMap
+        def dfs(x,y):
+            for i,j in [(0,1),(1,0),(0,-1),(-1,0)]:
+                nx,ny = x+i, y+j
+                if 0<=nx<len(grid) and 0<=ny<len(grid[0]) and grid[nx][ny] == "1":
+                    grid[nx][ny] = "0"
+                    dfs(nx,ny)
         res = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
-                    grid[i][j] = 0
-                    grid = bfs(grid,i,j)
+                    dfs(i,j)
                     res += 1
         return res
+                
+
 
                 
 
