eeb4b6e705df21b50230cdd9d09a2153d23dc83d
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-27 14:00:55 -0400

    Time: 4 ms (40.26%) | Memory: 17.7 MB (80.69%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,34 +1,50 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x<2:
-            return x
-        low = 2
-        high = x//2
-        while low <= high:
-            mid = low+(high - low)//2
-            val = mid**2
-            # print(val)
-            if val > x:
-                high = mid - 1
-            elif val < x:
-                low = mid + 1
-            else:
-                return mid
-        return high
+        # if x<2:
+        #     return x
+        # low = 2
+        # high = x//2
+        # while low <= high:
+        #     mid = low+(high - low)//2
+        #     val = mid**2
+        #     # print(val)
+        #     if val > x:
+        #         high = mid - 1
+        #     elif val < x:
+        #         low = mid + 1
+        #     else:
+        #         return mid
+        # return high
         # for i in range(0,x//2+2):
         #     if i**2 == x:
         #         return i
         #     if i**2 > x:
         #         return i-1
 
-        for i in range(0,x//2+2):
-            if i**2 == x:
-                return i
-            if i**2 > x:
-                return i-1
+        # for i in range(0,x//2+2):
+        #     if i*i == x:
+        #         return i
+        #     elif i*i > x:
+        #         return i-1
+        
+
+        if x < 2:
+            return x
+        
+        low = 1
+        high = x
+        while low <= high:
+            mid = low + (high-low)//2
+            if mid*mid == x:
+                return mid
+            if mid*mid < x:
+                low = mid+1
+            else:
+                high = mid -1
+        return high
 
 
-
+        
 
 
 
