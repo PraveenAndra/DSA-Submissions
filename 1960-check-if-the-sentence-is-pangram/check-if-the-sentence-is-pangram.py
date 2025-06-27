8e6ee2bcb2f505de409dd6695755cdcbb0dbde64
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-27 14:05:40 -0400

    Time: 0 ms (100.00%) | Memory: 17.5 MB (96.40%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,4 +1,12 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        return len(list(set(sentence))) == 26
+        # return len(set(sentence)) == 26
+        alph = [0]*26
+        for i in sentence:
+            alph[ord(i) - ord('a')] += 1
+        for i in alph:
+            if i==0:
+                return False
+        return True
+
         
