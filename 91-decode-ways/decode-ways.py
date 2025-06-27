e39486d55f3e6c71f648ab17010fb9c18fdd9e13
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-27 14:35:19 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (69.31%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -27,19 +27,30 @@ class Solution:
         #     return answer
         # return backtrack(0,s)
         
-        if s[0] == "0":
-            return 0
-        dp = [0]* (len(s)+1)
-        dp[1] = 1
-        dp[0] = 1
+        # if s[0] == "0":
+        #     return 0
+        # dp = [0]* (len(s)+1)
+        # dp[1] = 1
+        # dp[0] = 1
+        # for i in range(1,len(s)):
+        #     if s[i] != "0":
+        #         dp[i+1] = dp[i]
+        #     if int(s[i-1:i+1])>=10 and int(s[i-1:i+1]) <= 26:
+        #         dp[i+1] += dp[i-1]
+        # # print(dp)
+        # return dp[-1]
+        one_back = 1 if s[0]!="0" else 0
+        two_back = 1
         for i in range(1,len(s)):
+            curr = 0
             if s[i] != "0":
-                dp[i+1] = dp[i]
-            if int(s[i-1:i+1])>=10 and int(s[i-1:i+1]) <= 26:
-                dp[i+1] += dp[i-1]
-        # print(dp)
-        return dp[-1]
-
+                curr = one_back
+            two_digit = int(s[i-1:i+1])
+            if two_digit >= 10 and two_digit <=26:
+                curr += two_back
+            two_back = one_back
+            one_back = curr
+        return one_back
             
 
 
