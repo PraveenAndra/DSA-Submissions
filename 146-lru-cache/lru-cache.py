69b4bb6e95ca062875d2151195023169852a0d8b
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-27 16:24:51 -0400

    Time: 124 ms (73.96%) | Memory: 78.6 MB (27.48%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,51 +1,54 @@
-class Node:
-    def __init__(self,key,val):
-        self.prev = None
-        self.next = None
-        self.val= val
+class DLL:
+    def __init__(self,key,val=0,prev=None,next=None):
         self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.lookup = {}
-        self.head = Node(-1,-1)
-        self.tail = Node(-1,-1)
-        self.head.next= self.tail
+        self.head = DLL(-1)
+        self.tail = DLL(-1)
+        self.head.next = self.tail
         self.tail.prev = self.head
-        
+    def insertTail(self,node):
+        tailPrev = self.tail.prev
+        tailPrev.next = node
+        node.prev = tailPrev
+        node.next = self.tail
+        self.tail.prev = node
+    def removeHead(self):
+        removeNode = self.head.next
+        self.head.next = removeNode.next
+        removeNode.next.prev = self.head
+        return removeNode
+    def removeNode(self,node):
+        leftNode = node.prev
+        rightNode = node.next
+        leftNode.next = rightNode
+        rightNode.prev = leftNode
 
     def get(self, key: int) -> int:
         if key not in self.lookup:
             return -1
         node = self.lookup[key]
-        self.remove(node)
-        self.add(node)
+        self.removeNode(node)
+        self.insertTail(node)
         return node.val
 
     def put(self, key: int, value: int) -> None:
-        if key in self.lookup:
-            self.remove(self.lookup[key])
-        node = Node(key,value)
-        self.add(node)
+        if key in self.lookup:  
+            node = self.lookup[key]
+            self.removeNode(node)
+        node = DLL(key,value)
         self.lookup[key] = node
+        self.insertTail(node)
         if len(self.lookup) > self.capacity:
-            past = self.head.next
-            self.remove(past)
-            del self.lookup[past.key]
-
-    def remove(self,node):
-        prev_node = node.prev
-        next_node = node.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
-    def add(self,node):
-        prev_end = self.tail.prev
-        prev_end.next = node
-        node.next = self.tail
-        node.prev = prev_end
-        self.tail.prev = node
-
+            node = self.head.next
+            self.removeNode(node)
+            del self.lookup[node.key]
 
         
 
