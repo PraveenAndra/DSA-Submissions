043773878ff74a7c8baf0e15ff23796b01b8eac2
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-27 14:31:52 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (89.19%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,59 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # dp = [0 for i in range(len(s)+1)]
+        # dp[0] = 1
+        # dp[1] = 0 if s[0]=="0" else 1
+        # for i in range(2,len(dp)):
+        #     if dp[i-1]!=0:
+        #         dp[i] = dp[i-1]
+        #     two_digit = int(s[i-2:i])
+        #     if two_digit >= 10 and two_digit <=26:
+        #         dp[i] += dp[i-2]
+        # return dp[-1]
+
+
+    
+        # def backtrack(i,s):
+        #     if len(s) == i:
+        #         return 1
+        #     if s[i] == "0":
+        #         return 0
+        #     if i == len(s) - 1:
+        #         return 1
+        #     answer = backtrack(i+1,s)
+        #     if int(s[i:i+2]) <= 26:
+        #         answer += backtrack(i+2,s)
+        #     # one letter
+        #     return answer
+        # return backtrack(0,s)
+        
+        if s[0] == "0":
+            return 0
+        dp = [0]* (len(s)+1)
+        dp[1] = 1
+        dp[0] = 1
+        for i in range(1,len(s)):
+            if s[i] != "0":
+                dp[i+1] = dp[i]
+            if int(s[i-1:i+1])>=10 and int(s[i-1:i+1]) <= 26:
+                dp[i+1] += dp[i-1]
+        # print(dp)
+        return dp[-1]
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
