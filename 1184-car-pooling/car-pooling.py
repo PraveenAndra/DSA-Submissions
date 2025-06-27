85674d5b2875f7f8aad99c8ae083f19a374dbc3b
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-27 13:46:00 -0400

    Time: 4 ms (34.43%) | Memory: 18.3 MB (29.15%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -27,18 +27,27 @@ class Solution:
 
 
 
-        orders = []
-        for cap,fr,to in trips:
-            orders.append((fr,cap))
-            orders.append((to,-cap))
-        orders.sort()
-        curr = 0
-        for i in range(len(orders)):
-            curr += orders[i][1]
-            if curr > capacity:
+        # orders = []
+        # for cap,fr,to in trips:
+        #     orders.append((fr,cap))
+        #     orders.append((to,-cap))
+        # orders.sort()
+        # curr = 0
+        # for i in range(len(orders)):
+        #     curr += orders[i][1]
+        #     if curr > capacity:
+        #         return False
+        # return True
+        points = [0]*1001
+        for i in trips:
+            points[i[1]] += i[0]
+            points[i[2]] -= i[0]
+        change = 0
+        for i in points:
+            if i+change > capacity:
                 return False
+            change += i
         return True
-
 
 
 
