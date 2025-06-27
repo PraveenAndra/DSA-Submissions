6c409decb20ec3c7f22c3c5d66c04e351d0f6266
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-27 16:35:40 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (75.19%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,25 +1,25 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         # O(nlogn), O(n)
-        orders = []
-        for trip in trips:
-            orders.append((trip[1],trip[0]))
-            orders.append((trip[2],-trip[0]))
-        orders.sort()
-        curr = 0
-        for i,j in orders:
-            curr += j
-            if curr > capacity:
-                return False
-        return True
-        #O(n),O(l)
-        # timestamp = [0]*1001
+        # orders = []
         # for trip in trips:
-        #     timestamp[trip[1]] += trip[0]
-        #     timestamp[trip[2]] -= trip[0]
-        # change = 0
-        # for i in timestamp[1:]:
-        #     change += i
-        #     if change > capacity:
+        #     orders.append((trip[1],trip[0]))
+        #     orders.append((trip[2],-trip[0]))
+        # orders.sort()
+        # curr = 0
+        # for i,j in orders:
+        #     curr += j
+        #     if curr > capacity:
         #         return False
         # return True
+        #O(n),O(l)
+        timestamp = [0]*1001
+        for trip in trips:
+            timestamp[trip[1]] += trip[0]
+            timestamp[trip[2]] -= trip[0]
+        change = 0
+        for i in timestamp:
+            change += i
+            if change > capacity:
+                return False
+        return True
