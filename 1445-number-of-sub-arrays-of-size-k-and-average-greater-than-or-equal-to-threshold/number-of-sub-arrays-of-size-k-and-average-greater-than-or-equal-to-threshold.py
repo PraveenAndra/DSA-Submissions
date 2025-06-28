5d9b914747e4188f4569b496df3fc046e64ecff3
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-27 20:36:09 -0400

    Time: 79 ms (7.11%) | Memory: 28.3 MB (11.70%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,17 +1,11 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        res = 0
-        arr = [0]+arr
-        for i in range(1,k):
-            arr[i] = arr[i] + arr[i-1]
-        for i in range(k,len(arr)):
-            arr[i] = arr[i] + arr[i-1]
-            if (arr[i] - arr[i-k]) / k >= threshold:
+        for i in range(1,len(arr)):
+            arr[i] += arr[i-1]
+        res = 1 if arr[k-1]//k >= threshold else 0
+        if len(arr) == k:
+            return res
+        for i in range(len(arr)-k):
+            if (arr[i+k] - arr[i])/k >= threshold:
                 res += 1
         return res
-
-        # for i in range(len(arr)-k+1):
-        #     if sum(arr[i:i+k])/k >= threshold:
-        #         res+= 1
-        # return res
-        
