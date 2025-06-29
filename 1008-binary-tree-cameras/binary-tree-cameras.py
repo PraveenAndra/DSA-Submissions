555c438c1d478f2ef745a1e7ff4801d2fe3ca432
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-29 15:48:28 -0400

    Time: 3 ms (48.75%) | Memory: 18.2 MB (29.75%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.py
+++ 1008-binary-tree-cameras/binary-tree-cameras.py
@@ -6,18 +6,34 @@
 #         self.right = right
 class Solution:
     def minCameraCover(self, root: Optional[TreeNode]) -> int:
-        covered = {None}
+        # covered = {None}
+        # res = 0
+        # def dfs(node,par = None):
+        #     nonlocal res
+        #     if node:
+        #         dfs(node.left,node)
+        #         dfs(node.right,node)
+        #         if (par is None and node not in covered) or node.left not in covered or node.right not in covered:
+        #             res += 1
+        #             covered.update({node,node.left,node.right,par})
+        # dfs(root)
+        # return res
+        # 0 ffor not covered 1 for camera 2 for covered
         res = 0
-        def dfs(node,par = None):
+        def dfs(node):
             nonlocal res
-            if node:
-                dfs(node.left,node)
-                dfs(node.right,node)
-                if (par is None and node not in covered) or node.left not in covered or node.right not in covered:
-                    res += 1
-                    covered.update({node,node.left,node.right,par})
+            if not node:
+                return float('inf')
+            left = dfs(node.left)
+            right = dfs(node.right)
+            curr = min(node.left.val if node.left else float('inf'),node.right.val if node.right else float('inf'))
+            if curr == 0:
+                res += 1
+                node.val = 1
+            elif curr == 1:
+                node.val = 2
         dfs(root)
-        return res
+        return res+(1 if root.val == 0 else 0)
 
 
         
