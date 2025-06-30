44cac394d8a89dfdff5f3ca3a11e050e6bd5003a
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-30 17:03:44 -0400

    Time: 125 ms (78.46%) | Memory: 39.6 MB (52.79%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -6,26 +6,34 @@ class MedianFinder:
         
 
     def addNum(self, num: int) -> None:
-        if self.large and num > self.large[0]:
+        # if self.large and num > self.large[0]:
+        #     heapq.heappush(self.large,num)
+        # else:
+        #     heapq.heappush(self.small, -num)
+        # if len(self.small) > len(self.large)+1:
+        #     s = heapq.heappop(self.small)
+        #     heapq.heappush(self.large,-s)
+        # if len(self.large) > len(self.small)+1:
+        #     s = heapq.heappop(self.large)
+        #     heapq.heappush(self.small,-s)
+        if len(self.small) == len(self.large):
             heapq.heappush(self.large,num)
+            heapq.heappush(self.small,-heapq.heappop(self.large))
         else:
-            heapq.heappush(self.small, -num)
-        if len(self.small) > len(self.large)+1:
-            s = heapq.heappop(self.small)
-            heapq.heappush(self.large,-s)
-        if len(self.large) > len(self.small)+1:
-            s = heapq.heappop(self.large)
-            heapq.heappush(self.small,-s)
-        
+            heapq.heappush(self.small,-num)
+            heapq.heappush(self.large,-heapq.heappop(self.small))
 
     def findMedian(self) -> float:
-        if len(self.small) > len(self.large):
-            return -self.small[0]
-        elif len(self.large) > len(self.small):
-            return self.large[0]
+        # if len(self.small) > len(self.large):
+        #     return -self.small[0]
+        # elif len(self.large) > len(self.small):
+        #     return self.large[0]
+        # else:
+        #     return ((-self.small[0]+self.large[0])/2)
+        if len(self.small) == len(self.large):
+            return (self.large[0] - self.small[0])/2
         else:
-            return ((-self.small[0]+self.large[0])/2)
-
+            return -self.small[0]
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
