22f0a58b0d8fd64e6fbbcdfe49f31bfdfca4ed1a
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-30 21:58:19 -0400

    Time: 3 ms (87.59%) | Memory: 19.8 MB (75.18%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,18 +1,13 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        available = 0
-        start = []
+        curr_rooms = []
+        intervals.sort()
+        min_num = 0
         for i,j in intervals:
-            start.append((i,1))
-            start.append((j,-1))
-        start.sort()
-        curr = 0
-        for i,j in start:
-            curr += j
-            available = max(available,curr)
-        return available
-        
-
-
-
+            if curr_rooms:
+                if curr_rooms[0] <= i:
+                    heapq.heappop(curr_rooms)
+            heapq.heappush(curr_rooms,j)
+            min_num = max(min_num,len(curr_rooms))
+        return min_num
         
