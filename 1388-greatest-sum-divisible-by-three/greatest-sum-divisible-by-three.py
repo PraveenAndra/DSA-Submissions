674d43e9e38b7de24fedb7a9e5222b947c0a9758
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-02 10:07:19 -0400

    Time: 109 ms (35.19%) | Memory: 21.7 MB (69.50%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
@@ -3,13 +3,26 @@ class Solution:
         dp = [0, -math.inf, -math.inf]
         for x in nums:
             m = x % 3
-            new_dp = dp[:]  # copy current bests
+            new_dp = dp[:]
             for r in range(3):
-                # try adding x to a sum that was ≡ r
                 cand = dp[r] + x
                 new_dp[(r + m) % 3] = max(new_dp[(r + m) % 3], cand)
             dp = new_dp
         return dp[0]
+        # s = 0
+        # buckets = [float('inf'),float('inf'),float('inf')]
+        # for i in nums:
+        #     rem = i%3
+        #     buckets[rem] = min(buckets[rem],i)
+        #     s += i
+        # print(s,buckets)
+        # if s%3 == 0:
+        #     s = s 
+        # elif s%3 == 1:
+        #     s = s- buckets[1]
+        # else:
+        #     s = s - buckets[2]
+        # return s if s != float('inf') else 0 
 
 
         
