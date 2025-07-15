a27411e441d6d4ad5eae9615839abbb5d5864536
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-15 12:11:18 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (69.25%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -4,18 +4,14 @@ class Solution:
         def backtrack(ind,arr,openCount,closedCount):
             if ind == 2*n:
                 res.append(''.join(arr))
-            if openCount==0:
+            if openCount < n:
                 arr.append("(")
                 backtrack(ind+1,arr,openCount+1,closedCount)
-            else:
-                if openCount < n:
-                    arr.append("(")
-                    backtrack(ind+1,arr,openCount+1,closedCount)
-                    arr.pop()
-                if closedCount < openCount:
-                    arr.append(")")
-                    backtrack(ind+1,arr,openCount,closedCount+1)
-                    arr.pop()
+                arr.pop()
+            if closedCount < openCount:
+                arr.append(")")
+                backtrack(ind+1,arr,openCount,closedCount+1)
+                arr.pop()
         backtrack(0,[],0,0)
         return res
             
