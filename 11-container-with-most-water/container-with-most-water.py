91a5423de8afb65929c480a04253e7ace90ebba4
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-18 18:15:05 -0400

    Time: 71 ms (88.94%) | Memory: 28.6 MB (23.82%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,22 +1,13 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        #Brute Force(TLE)
-        # res = 0
-        # for i in range(len(height)):
-        #     for j in range(i,len(height)):
-        #         area = min(height[i],height[j]) * (j-i)
-        #         res = max(res,area)
-        # return res
-
-        #Two Pointer
-        left,right = 0 ,len(height)-1
+        left = 0
+        right = len(height) - 1
         res = 0
         while left < right:
-            area = min(height[left],height[right]) * (right-left)
-            res = max(res,area)
             if height[left] < height[right]:
+                res = max(res,height[left]*(right-left))
                 left += 1
             else:
+                res = max(res,height[right]*(right-left))
                 right -= 1
         return res
-            
