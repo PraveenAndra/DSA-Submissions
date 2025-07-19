b39bebe93c33e1a9e1853c090967a80a85d13466
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-18 21:58:48 -0400

    Time: 252 ms (49.45%) | Memory: 20.3 MB (42.73%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(x,y):
-            for i,j in [(0,1),(1,0),(0,-1),(-1,0)]:
-                nx,ny = x+i, y+j
-                if 0<=nx<len(grid) and 0<=ny<len(grid[0]) and grid[nx][ny] == "1":
-                    grid[nx][ny] = "0"
-                    dfs(nx,ny)
-        res = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        r,c = len(grid),len(grid[0])
+        def traverseIsland(a,b):
+            for i,j in [(0,1),(1,0),(-1,0),(0,-1)]:
+                nr,nc = a+i,b+j
+                if 0<=nr<r and 0<=nc<c and grid[nr][nc] == "1":
+                    grid[nr][nc] = 0
+                    traverseIsland(nr,nc)
+        islands = 0
+        for i in range(r):
+            for j in range(c):
                 if grid[i][j] == "1":
-                    dfs(i,j)
-                    res += 1
-        return res
-                
+                    grid[i][j] = 0
+                    traverseIsland(i,j)
+                    islands += 1
+        return islands
 
 
-                
-
-        
-             
-
-
-
-        
-         
-        
         
