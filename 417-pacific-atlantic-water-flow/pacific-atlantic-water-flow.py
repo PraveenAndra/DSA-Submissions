9f9eac8025a4f26d7160d702369dd4162ebd8a4b
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-19 18:21:18 -0400

    Time: 55 ms (18.98%) | Memory: 19.3 MB (65.26%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,27 +1,30 @@
 class Solution:
+
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        q1,q2 = deque(),deque()
-        for i in range(len(heights)):
-            q1.append((i, 0))
-            q2.append((i, len(heights[0]) - 1))
-        for i in range(len(heights[0])):
-            q1.append((0, i))
-            q2.append((len(heights) - 1, i))
+        if not heights or not heights[0]:
+            return 0
+        pacific_nodes = deque()
+        atlantic_nodes = deque()
+        r,c = len(heights),len(heights[0])
+        for i in range(r):
+            pacific_nodes.append((i,0))
+            atlantic_nodes.append((i,c-1))
+        for i in range(c):
+            pacific_nodes.append((0,i))
+            atlantic_nodes.append((r-1,i))
         
-        def bfs(q):
-            res = set()
+        def check_cells(q):
+            visited = set()
             while q:
-                r,c = q.popleft()
-                res.add((r,c))
-                for i,j in [(0,1),(1,0),(0,-1),(-1,0)]:
-                    nr,nc = r+i,c+j
-                    if 0<=nr<len(heights) and 0<=nc<len(heights[0]) and (nr,nc) not in res and heights[nr][nc] >= heights[r][c]:
-                        q.append((nr,nc))
-            return res
-        pacific = bfs(q1)
-        atlantic = bfs(q2)
+                i,j = q.popleft()
+                visited.add((i,j))
+                for a,b in [(0,1),(1,0),(-1,0),(0,-1)]:
+                    ni,nj = i+a,j+b
+                    if 0<=ni<len(heights) and 0<=nj<len(heights[0]) and heights[ni][nj] >= heights[i][j] and (ni,nj) not in visited:
+                        q.append((ni,nj))
+            return visited
+        
+        pacific = check_cells(pacific_nodes)
+        atlantic = check_cells(atlantic_nodes)
         return list(pacific.intersection(atlantic))
 
-
-
-        
