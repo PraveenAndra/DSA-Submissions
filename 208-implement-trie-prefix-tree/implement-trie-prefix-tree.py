5234288dc81ffb995d7c0ece8115b4db40a6294f
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-19 16:43:11 -0400

    Time: 60 ms (30.00%) | Memory: 32 MB (75.86%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -2,40 +2,33 @@ class TrieNode:
     def __init__(self):
         self.trie = {}
         self.last = False
-
 class Trie:
 
     def __init__(self):
-        self.node = TrieNode()
-        
+        self.trie = TrieNode() 
 
     def insert(self, word: str) -> None:
-        curr = self.node
-        for ind in range(len(word)):
-            i = word[ind]
-            if i not in curr.trie:
-                curr.trie[i] = TrieNode()
-            
-            curr = curr.trie[i]
-            if ind == len(word)-1:
-                curr.last = True
-            
-
-
+        curr = self.trie
+        for c in word:
+            if c not in curr.trie:
+                curr.trie[c] = TrieNode()
+            curr = curr.trie[c]
+        curr.last = True
+        
     def search(self, word: str) -> bool:
-        curr = self.node
-        for i in range(len(word)):
-            if word[i] not in curr.trie:
+        curr = self.trie
+        for c in word:
+            if c not in curr.trie:
                 return False
-            curr = curr.trie[word[i]]
-        return curr.last 
+            curr = curr.trie[c]
+        return curr.last
 
     def startsWith(self, prefix: str) -> bool:
-        curr = self.node
-        for i in range(len(prefix)):
-            if prefix[i] not in curr.trie:
+        curr = self.trie
+        for c in prefix:
+            if c not in curr.trie:
                 return False
-            curr = curr.trie[prefix[i]]
+            curr = curr.trie[c]
         return True
         
 
