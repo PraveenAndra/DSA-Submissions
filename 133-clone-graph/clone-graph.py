d95c7bcb1e18e83e34468f1aa9572576fdf3f032
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-20 16:03:52 -0400

    Time: 38 ms (86.10%) | Memory: 18.4 MB (16.20%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,6 +9,7 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+
         if not node:
             return None
         visited = {}
