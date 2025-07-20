a4800f4cf3101caaedd32ac8e39cc251c3f36731
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-19 21:13:28 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (38.04%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,34 +1,17 @@
-class DisjointSet:
-    def __init__(self,n):
-        self.parent = [i for i in range(n)]
-        self.rank = [0]*n
-    def find(self,A):
-        if self.parent[A]!= A:
-            self.parent[A] = self.find(self.parent[A])
-        return self.parent[A]
-    def union(self,node1,node2):
-        parent1 = self.find(node1)
-        parent2 = self.find(node2)
-        if parent1 != parent2:
-            if self.rank[parent1] < self.rank[parent2]:
-                self.parent[parent1] = self.parent[parent2]
-            elif self.rank[parent2] < self.rank[parent1]:
-                self.parent[parent2] = self.parent[parent1]
-            else:
-                self.parent[parent2] = self.parent[parent1]
-                self.rank[parent1] += 1
-            return True
-        else:
-            return False
-
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        ds = DisjointSet(n)
-        if len(edges)< n -1:
-            return False
-        for i,j in edges:
-            if not ds.union(i,j):
-                return False
-        return True
-        
-        
+        if len(edges) != n - 1: return False
+        adj_list = [[] for _ in range(n)]
+        for A, B in edges:
+            adj_list[A].append(B)
+            adj_list[B].append(A)
+        seen = {0}
+        stack = [0]
+        while stack:
+            node = stack.pop()
+            for k in adj_list[node]:
+                if k not in seen:
+                    stack.append(k)
+                    seen.add(k)
+        print(seen)
+        return len(seen) == n
