54e038ea8a323faaffc7581583eed4d5021373d0
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-19 20:22:59 -0400

    Time: 3274 ms (78.27%) | Memory: 17.9 MB (64.94%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,25 +1,21 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def backtrack(x,y,visited,ind):
+        def search(x,y,ind):
             if ind == len(word):
                 return True
+            val = board[x][y]
+            board[x][y] = "#"
             for i,j in [(0,1),(1,0),(-1,0),(0,-1)]:
-                nx,ny = x+i,j+y
-                if 0<=nx<len(board) and 0<=ny<len(board[0]) and (nx,ny) not in visited and word[ind] == board[nx][ny]:
-                    visited.add((nx,ny))
-                    if backtrack(nx,ny,visited,ind+1):
+                nx,ny = x+i,y+j
+                if 0<=nx<len(board) and 0<=ny<len(board[0]) and board[nx][ny] == word[ind]:
+                
+                    if search(nx,ny,ind+1):
                         return True
-                    visited.remove((nx,ny))
+            board[x][y] = val
             return False
-
-
-        stack = []
         for i in range(len(board)):
             for j in range(len(board[0])):
-                visited = set()
-                visited.add((i,j))
-                if board[i][j] == word[0] and backtrack(i,j,visited,1):
-                    return True
+                if board[i][j] == word[0]:
+                    if search(i,j,1):
+                        return True
         return False
-
-        
