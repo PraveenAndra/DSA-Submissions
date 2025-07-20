0c1e87d78cc0aedf77421e7085a38cc8cb51081f
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-20 16:40:54 -0400

    Time: 3 ms (70.83%) | Memory: 18.4 MB (7.20%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,14 +1,30 @@
+class TrieNode:
+    def __init__(self):
+        self.trie = {}
+        self.last = False
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        @cache
-        def dfs(ind):
-            if ind == len(s):
-                return True
-            if ind >= len(s):
-                return False
-            for i in range(ind+1,len(s)+1):
-                if s[ind:i] in wordDict and dfs(i):
-                    return True
-            return False
-        return dfs(0)
+        root = TrieNode()
+        for word in wordDict:
+            curr = root
+            for char in word:
+                if char not in curr.trie:
+                    curr.trie[char] = TrieNode()
+                curr = curr.trie[char]
+            curr.last = True
+        l = 0
+        dp = [False]*len(s)
+        for i in range(len(s)):
+            if i==0 or dp[i-1]:
+                curr = root
+                for j in range(i,len(s)):
+                    if s[j] in curr.trie:
+                        curr = curr.trie[s[j]]
+                    else:
+                        break
+                    if curr.last:
+                        dp[j] = True
+        return dp[-1]
+                
+
         
