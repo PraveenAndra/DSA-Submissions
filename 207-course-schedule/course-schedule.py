ce652e63eaada00a2a2b5f73b39ca08b7b3f8799
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-21 12:13:44 -0400

    Time: 0 ms (100.00%) | Memory: 19 MB (83.38%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,58 +1,47 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-
-        adj_list = [[] for i in range(numCourses)]
-        
-
-
-        # # DFS Solution
-        # for i,j in prerequisites:
-        #     adj_list[i].append(j)
-        # def dfs(curr,visited,stack):
-        #     if stack[curr]:
-        #         return True
-        #     if visited[curr]:
-        #         return False
-        #     visited[curr] = True
-        #     stack[curr] = True
-        #     for i in adj_list[curr]:
-        #         if dfs(i,visited,stack):
-        #             return True
-        #     stack[curr] = False
-        #     return False
-        
-        # visited = [False for i in range(numCourses)]
-        # stack = [False for i in range(numCourses)]
-
-        # for i in range(numCourses):
-        #     if dfs(i,visited,stack):
-        #         return False
-        # return True
-
-        #Topological Sort
-        inorder = [0]*numCourses
+        # visited = set()
+        adj_list = defaultdict(list)
+        indegree = [0]*numCourses
         for i,j in prerequisites:
-            adj_list[i].append(j)
-            inorder[j] += 1
-        stack = deque()
+            adj_list[j].append(i)
+            indegree[i] += 1
+        
+        q = deque()
         for i in range(numCourses):
-            if inorder[i] == 0:
-                stack.append(i)
-        res = 0
-        visited = [False]*numCourses
-        while stack:
-            curr = stack.popleft()
-            if visited[curr]:
-                continue
-            res += 1
-            visited[curr] = True
-            for i in adj_list[curr]:
-                inorder[i] -= 1
-                if inorder[i] == 0:
-                    stack.append(i)
-        return res == numCourses
+            if indegree[i] == 0:
+                q.append(i)
+        order = []
+        while q:
+            course = q.popleft()
+            for i in adj_list[course]:
+                indegree[i] -= 1
+                if indegree[i] == 0:
+                    q.append(i)
+            order.append(course)
+        return len(order) == numCourses
+            
 
-
-
-
+        
+        # order = []
+        # def dfs(curr,visited,inStack):
+        #     if inStack[curr]:
+        #         return True
+        #     if curr in visited:
+        #         return False
+        #     inStack[curr] = True
+        #     for n in adj_list[curr]:
+        #         if dfs(n,visited,inStack):
+        #             return True
+        #     visited.add(curr)
+        #     order.append(curr)
+        #     inStack[curr] = False
+        #     return False
+        # inStack = [False]*numCourses
+        # # for i in range(numCourses):
+        # #     if dfs(i,visited,inStack):
+        # #         return False
+        # dfs(1,visited,inStack)
+        # print(order)
+        # return True
         
