3997f46b967b48514cbe71994b58dedcbf824d76
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-22 19:26:24 -0400

    Time: 11 ms (78.48%) | Memory: 18.1 MB (38.62%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,28 +1,16 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # l = len(nums)
-        # dp = [[0]*nums for i in range(l)]
-        # for i in range(l):
-        #     for j in range(l):
-        #         if 
-        tails = [0] * len(nums)
-        result = 0
+        sub = [nums[0]]
         for num in nums:
-            left_index, right_index = 0, result
-            # print("Before Loop")
-            print(num,left_index,right_index)
-            # print("In loop")
-            while left_index != right_index:
-                middle_index = left_index + (right_index - left_index) // 2
-                if tails[middle_index] < num:
-                    left_index = middle_index + 1
+            left,right = 0,len(sub)
+            while left < right:
+                mid = left + (right-left)//2
+                if sub[mid] < num:
+                    left = mid+1
                 else:
-                    right_index = middle_index
-                # print(left_index,middle_index,right_index)
-            result = max(result, left_index + 1)
-            # print("Result "+str(result))
-            tails[left_index] = num
-            # print(tails)
-        return result
-
-        
+                    right = mid
+            if left >= len(sub):
+                sub.append(num)
+            else:
+                sub[left] = num
+        return len(sub)
