00126c6779d8265b42f5474a51f9d6c1ce9d0ac2
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-24 11:51:21 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (26.13%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,14 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [0]*(n+1)
-        dp[1] = 1
+        matrix= [[0]*n for i in range(m)]
+        for i in range(n):
+            matrix[0][i] = 1
         for i in range(m):
-            temp = [0]*(n+1)
-            for j in range(1,n+1):
-                temp[j] = temp[j-1]+dp[j]
-            dp = temp
-        return dp[n]
-                
+            matrix[i][0] = 1
+        
+        for i in range(1,m):
+            for j in range(1,n):
+                # print(i,j)
+                matrix[i][j] = matrix[i-1][j]+matrix[i][j-1]
+        return matrix[m-1][n-1]
 
-                
+        
+
         
