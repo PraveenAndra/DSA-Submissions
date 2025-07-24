88f6d67d2206afd0d48b9ca6285fa2c695f4d7ba
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-24 15:28:44 -0400

    Time: 7 ms (53.10%) | Memory: 19.2 MB (44.24%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,58 +1,38 @@
-class DisjoinSet:
+class DisjointSet:
     def __init__(self,n):
         self.parent = [i for i in range(n)]
-        self.size = [1]*n
+        self.rank = [0]*n
+        self.comps = 0
+    def find(self,node):
+        if self.parent[node] != node:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+    def union(self,node1,node2):
+        parent1 = self.find(node1)
+        parent2 = self.find(node2)
+        if parent1 != parent2:
+            if self.rank[parent1] < self.rank[parent2]:
+                self.parent[parent1] = self.parent[parent2]
+            elif self.rank[parent1] > self.rank[parent2]:
+                self.parent[parent2] = self.parent[parent1]
+            else:
+                self.rank[parent1] += 1
+                self.parent[parent2] = self.parent[parent1]
+            self.comps += 1
+            return True
+        return False
     
-    def find(self,n):
-        if n!=self.parent[n]:
-            self.parent[n] = self.find(self.parent[n])
-        return self.parent[n]
-    def union(self,A,B):
-        rootA = self.find(A)
-        rootB = self.find(B)
-        if rootA == rootB:
-            return 0
-        if self.size[rootA] < self.size[rootB]:
-            self.size[rootB] += self.size[rootA]
-            self.parent[rootA] = rootB
-        else:
-            self.size[rootA] += self.size[rootB]
-            self.parent[rootB] = rootA
-        return 1
+
 
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        # Iterative DFS
-        # adj_mat = defaultdict(list)
-        # for i,j in edges:
-        #     adj_mat[i].append(j)
-        #     adj_mat[j].append(i)
-        # visited = [False]*n
-        # def iter_dfs(node,visited,adj_mat):
-        #     stack = [node]
-        #     visited[node] = True
-        #     for i in adj_mat[node]:
-        #         if not visited[i]:
-        #             iter_dfs(i,visited,adj_mat)
-        #     return
-        # res = 0
-        # for i in range(n):
-        #     if not visited[i]:
-        #         iter_dfs(i,visited,adj_mat)
-        #         res += 1
-        
-        # return res
 
-        #DSU
-        dsu1 = DisjoinSet(n)
-        dsu2 = DisjoinSet(n)
-        res = n
+        dsu = DisjointSet(n)
+
         for i,j in edges:
-            res -= dsu1.union(i,j)
-        return res
-            
-
-
-
+            dsu.union(i,j)
+                # n-=1
+        print(dsu.comps)
+        return n - dsu.comps
 
         
