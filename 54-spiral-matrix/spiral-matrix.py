5d3e9fa67bb85de4311b25d9f39f94aa1a5456d2
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-30 18:50:34 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (61.93%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,32 +1,27 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        dir = [(0,1),(1,0),(0,-1),(-1,0)]
-        i,j = 0,0
-        r,c = len(matrix),len(matrix[0])
         res = []
-        up,down,left,right = 0,r-1,0,c-1
-        while len(res) < r*c:
-            # Top row
-            for j in range(left,right+1):
-                res.append(matrix[up][j])
-            # Right Col
+        top = 0
+        bottom = len(matrix) - 1
+        right = len(matrix[0])-1
+        left = 0
+        while len(res) < len(matrix)*len(matrix[0]):
+            # Move right
+            for i in range(left,right+1):
+                res.append(matrix[top][i])
             
-            for i in range(up+1,down+1):
+            for i in range(top+1,bottom +1):
                 res.append(matrix[i][right])
-            
-            if up != down:
+            if top != bottom:
                 for i in range(right-1,left-1,-1):
-                    res.append(matrix[down][i])
-            
-            if left!=right:
-                for i in range(down-1,up,-1):
+                    res.append(matrix[bottom][i])
+            if left != right:
+                for i in range(bottom-1,top,-1):
                     res.append(matrix[i][left])
-            up += 1       
-            left += 1
-            down -= 1
+            bottom -= 1
             right -= 1
+            top += 1
+            left += 1
         return res
-
-            
 
         
