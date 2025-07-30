2cdb0e7994abfd4db052b6d82e3d69c588bcc310
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-30 01:07:26 -0400

    Time: 8 ms (9.67%) | Memory: 18.2 MB (38.87%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,30 +1,19 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
-        m, n = len(nums1), len(nums2)
-
-        lo, hi = 0, m
-        while lo <= hi:
-            i = (lo + hi) // 2                 # cut in nums1
-            j = (m + n + 1)//2 - i             # matching cut in nums2
-
-            L1 = -float('inf') if i == 0 else nums1[i-1]
-            R1 =  float('inf') if i == m else nums1[i]
-            L2 = -float('inf') if j == 0 else nums2[j-1]
-            R2 =  float('inf') if j == n else nums2[j]
-
-            if L1 <= R2 and L2 <= R1:           # correct partition
-                if (m + n) & 1:                # odd length
-                    return float(max(L1, L2))
-                return (max(L1, L2) + min(R1, R2)) / 2.0
-            elif L1 > R2:                      # move cut in nums1 left
-                hi = i - 1
-            else:                              # move cut in nums1 right
-                lo = i + 1
-
-        return -1.0 
-
-
-
-        
+        l1,l2 = 0,0
+        res = []
+        while l1 < len(nums1) and l2 < len(nums2):
+            if nums1[l1] < nums2[l2]:
+                res.append(nums1[l1])
+                l1 += 1
+            else:
+                res.append(nums2[l2])
+                l2 += 1
+        if l1 < len(nums1):
+            res.extend(nums1[l1:])
+        if l2 < len(nums2):
+            res.extend(nums2[l2:])
+        mid = (len(nums1)+len(nums2))//2
+        if (len(nums1)+len(nums2)) %2 == 0:
+            return (res[mid-1]+res[mid])/2
+        return res[mid]
