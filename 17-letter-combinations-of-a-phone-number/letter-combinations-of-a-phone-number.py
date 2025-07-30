d31b80d5511ac476b755f7374af037df5c467751
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-07-30 02:30:36 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (23.04%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,16 +1,17 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-
+        numMap = {"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
         res = []
-        num_map = {2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
         def backtrack(ind,curr):
             if ind == len(digits):
                 res.append(curr)
                 return
-            for letter in num_map[int(digits[ind])]:
-                backtrack(ind+1,curr+letter)
+            for i in numMap[digits[ind]]:
+                backtrack(ind+1,curr+i)
         if digits == "":
             return []
         backtrack(0,"")
         return res
+
+
         
