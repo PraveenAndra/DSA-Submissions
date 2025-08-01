267a251c40736490dac51af0d9098091f6a2aac7
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-08-01 12:11:42 -0400

    Time: 99 ms (36.80%) | Memory: 28.7 MB (45.28%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,34 +1,8 @@
 class Solution:
-    def findKthLargest(self, nums, k):
-        # arr = []
-        # for i in nums:
-        #     if len(arr) == k and i > arr[0]:
-        #         heapq.heappop(arr)
-        #         heapq.heappush(arr,i)
-        #     elif len(arr) < k:
-        #         heapq.heappush(arr,i)
-        # return arr[0]
-
-        def quick_select(nums, k):
-            pivot = random.choice(nums)
-            left, mid, right = [], [], []
-
-            for num in nums:
-                if num > pivot:
-                    left.append(num)
-                elif num < pivot:
-                    right.append(num)
-                else:
-                    mid.append(num)
-            
-            if k <= len(left):
-                return quick_select(left, k)
-            
-            if len(left) + len(mid) < k:
-                return quick_select(right, k - len(left) - len(mid))
-            
-            return pivot
-        
-        return quick_select(nums, k)
-
-                
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        for num in nums:
+            heapq.heappush(heap,num)
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return heap[0]        
