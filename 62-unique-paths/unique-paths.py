49c6a557b7fa5810b4a6d9afa0c8c01dc130bd0f
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-08-04 22:43:09 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.62%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,7 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        if m== 1 and n==1:
+            return 1
         grid = [[0]*n for i in range(m)]
-        grid[0][0] = 1
         for i in range(1,n):
             grid[0][i] = 1
         for i in range(1,m):
