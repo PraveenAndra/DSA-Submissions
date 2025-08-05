ccb0e2db3e4fa50d5975e0a4a301dcdd5153fb4a
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-08-04 22:25:32 -0400

    Time: 567 ms (87.50%) | Memory: 82.1 MB (73.66%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,32 +1,20 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        N = len(points)
-        adj = {i: [] for i in range(N)}
-        for i in range(N):
-            x1, y1 = points[i]
-            for j in range(i + 1, N):
-                x2, y2 = points[j]
-                dist = abs(x1 - x2) + abs(y1 - y2)
-                adj[i].append([dist, j])
-                adj[j].append([dist, i])
-        
-        q = []
-        q.append((0,0))
-        visited = [0]*N
-        min_dist = [0]*N
+        n = len(points)
+        heap = [(0,0)]
+        visited = [False]*n
         cost = 0
-        while q:
-            res, point = heapq.heappop(q)
-            if visited[point] !=0:
+        edges = 0
+        while edges < n:
+            weight,node = heapq.heappop(heap)
+            if visited[node]:
                 continue
-            cost += res
-            visited[point] = 1
-            for dist,j in adj[point]:
-                if visited[j] == 0:
-                    heapq.heappush(q,(dist,j))
-        return cost
-                
-            
-
-
+            visited[node] = True
+            cost += weight
+            edges += 1
+            for next in range(n):
+                if not visited[next]:
+                    next_weight = abs(points[node][0] - points[next][0]) + abs(points[node][1] - points[next][1])
+                    heapq.heappush(heap,(next_weight,next))
         
+        return cost
